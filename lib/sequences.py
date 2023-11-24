986c4ee62d40ae3bce6508539c99ec03514562ec
Juliet <musyoki.juliet011@gmail.com>
2023-11-24 11:26:51 +0300

    DONE

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    my_list = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        my_list.append(a)
+        a, b = b, a + b
+
+    print(my_list)
+    
